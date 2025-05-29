e557dfdb3b22b15558e0a1bb5a9821f2122d69f9
Arsema Addis <arsemadd@gmail.com>
2025-05-29 21:30:08 +0300

    Create Request and Response DTOs

---
+++ src/main/java/com/example/bankingmanagement/payload/request/LoginRequest.java
@@ -0,0 +1,17 @@
+package com.example.bankingmanagement.payload.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    // Getters and Setters (Lombok @Getter, @Setter will do this automatically)
+}
